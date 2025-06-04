4281bf8a204ddec34cb080479dafb28360d274ec
Sohel Sheikh <sohelxsheikh@gmail.com>
2025-06-04 16:58:16 +0530

    Doto

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'], // adjust as needed
+  display: 'swap',
+  variable: "--font-poppins"
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -20,7 +27,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
       >
         {children}
       </body>
---
+++ src/app/page.jsx
@@ -0,0 +1,21 @@
+import Todo from "@/components/todo";
+import { cn } from "@/lib/utils";
+
+export default function Home() {
+  return (
+   <div className="relative flex h-[50rem] w-full items-center justify-center bg-gray-100 dark:bg-gray-900">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:20px_20px]",
+          "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+          "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+        )}
+      />
+      {/* Radial gradient for the container to give a faded look */}
+     <div className="font-[poppins] w-full h-full relative">
+       <Todo/>
+     </div>
+    </div>
+  );
+}
